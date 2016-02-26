2ea05b34a703c0f5db5ee0683b909331164150e2
sefp2012 <sefp2012@yandex.ru>
2016-02-26 11:00:45 +0300

    Add About Us page and rspec test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
   describe "Home page" do
 
@@ -11,11 +11,18 @@ describe "StaticPages" do
   end
 
   describe "Help page" do
-    
-    it "should have content 'Help'" do
+
+    it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
   end
 
-end
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+end 
